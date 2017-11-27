2b3c62c7b67c93c0a91443e0d063fabb4b1c2f80
chidozie <chidough14@gmail.com>
2017-11-27 20:10:52 +0100

    first commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,31 @@
+
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import Vue from 'vue'
+import VueChatScroll from 'vue-chat-scroll'
+Vue.use(VueChatScroll)
+
+
+Vue.component('example', require('./components/Example.vue'));
+Vue.component('message', require('./components/Message.vue'));
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: '',
+        chat: {
+            message: []
+        }
+    },
+    methods: {
+      send() {
+          if (this.message.length != 0) {
+              this.chat.message.push(this.message);
+              this.message= '';
+          }
+      }
+    }
+});
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style>
+       .list-group {
+           overflow-y: scroll;
+           height: 200px;
+       }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="row" id="app">
+        <div class=" offset-4 col-4">
+            <li class="list-group-item active">Chat Room</li>
+        <ul class="list-group" v-chat-scroll>
+            <Message
+               v-for="value in chat.message"
+               :key="value.index"
+                color="success">
+                @{{ value }}
+            </Message>
+        </ul>
+            <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Type your message...."
+                    v-model="message"
+                    @keyup.enter="send">
+        </div>
+    </div>
+</div>
+
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
