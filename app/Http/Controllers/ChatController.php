abf0495e7a434f8a43bef65bfc3ee96655c6fb7e
chidozie <chidough14@gmail.com>
2017-11-27 20:22:22 +0100

    second commit

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -12,6 +12,7 @@ class ChatController extends Controller
     public function __construct()
     {
        $this->middleware('auth');
+
     }
 
     public function chat()
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Route::get('chat', 'ChatController@chat');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index');
