b7a271633cd3275131892ff78825d3840743ea5d
chidozie <chidough14@gmail.com>
2017-11-28 05:47:08 +0100

    second commit

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -20,9 +20,17 @@ class ChatController extends Controller
         return view('chat');
     }
 
-    public function send(Request $request)
+   /* public function send(Request $request)
     {
         $user= User::find(Auth::id());
        event(new ChatEvent($request->message, $user));
+    }*/
+
+    public function send()
+    {
+        $message= 'Hello';
+       $user= User::find(Auth::id());
+       $wr= broadcast(new ChatEvent($message, $user));
+       dd($wr);
     }
 }
